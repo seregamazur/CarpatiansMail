9c5ad488afed083b6cdba8ac4435cadb07a594f8
Igor-Polatajko <igor.bogdanovich39@gmail.com>
2019-01-10 17:09:28 +0200

    parser from xls

---
+++ src/main/java/employee/Employee.java
@@ -0,0 +1,32 @@
+package xlsParser;
+
+public class Employee {
+		private String name;
+		private String position;
+		private String eMail;
+		private int level;
+		
+		
+		Employee(String name, String position, String eMail, int level){
+			this.name = name;
+			this.position = position;
+			this.eMail = eMail;
+			this.level = level;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public String getPosition() {
+			return position;
+		}
+		
+		public String getEmail() {
+			return eMail;
+		}
+		
+		public int getLevel() {
+			return level;
+		}
+}
