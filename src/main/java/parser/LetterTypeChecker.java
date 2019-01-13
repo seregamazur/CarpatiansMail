d8f5e33adfa6bdde667c24a97888aa413103ac8a
Maria <slobodjanmarija1@gmail.com>
2019-01-14 01:36:08 +0200

    Beta v1.2

--- src/main/java/parser/LetterTypeChecker.java
+++ src/main/java/parser/LetterTypeChecker.java
@@ -1,5 +1,22 @@
 package parseJSON;
-
+import java.util.Arrays;
 public class LetterTypeChecker {
 
+	
+	public LetterType IsRequest(String content, boolean isSttachment) {
+		
+		String str = content.trim();
+		String [] arr = str.split(" ");
+		
+        if(arr.length==2 && isSttachment == false){
+        		return LetterType.ANSWER;	
+        }else if(arr.length>3 && isSttachment == true) {
+        	return LetterType.REQUEST;
+        	
+            }else {
+        	   return LetterType.UNDEFINED;
+            }
+	}	
 }
+
+
