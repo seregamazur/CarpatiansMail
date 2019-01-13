311a01bd59b85d900051ba66b884e26f21282c57
Maria <slobodjanmarija1@gmail.com>
2019-01-14 01:42:20 +0200

    Beta v1.2

--- src/main/java/parser/LetterTypeChecker.java
+++ src/main/java/parser/LetterTypeChecker.java
@@ -3,19 +3,19 @@ import java.util.Arrays;
 public class LetterTypeChecker {
 
 	
-	public LetterType IsRequest(String content, boolean isSttachment) {
+	public LetterType IsRequest(String content, boolean isAttachment) {
 		
 		String str = content.trim();
 		String [] arr = str.split(" ");
 		
-        if(arr.length==2 && isSttachment == false){
-        		return LetterType.ANSWER;	
-        }else if(arr.length>3 && isSttachment == true) {
-        	return LetterType.REQUEST;
+        if(arr.length==2 && !isAttachment){
+        	  return LetterType.ANSWER;	
+        }else if(arr.length>3 && isAttachment) {
+        	      return LetterType.REQUEST;
         	
-            }else {
-        	   return LetterType.UNDEFINED;
-            }
+               }else {
+        	       return LetterType.UNDEFINED;
+               }
 	}	
 }
 
