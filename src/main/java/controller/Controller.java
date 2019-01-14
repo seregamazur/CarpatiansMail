047b99bf9862b459622abf0138c2cf1877bffd28
seregamazur <serhiy.mazur0@gmail.com>
2019-01-14 02:20:29 +0200

    Added receiver

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,9 +1,9 @@
 package controller;
 
-import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
-
+import client.authenticator.EmailAuthenticator.Gmail;
+import client.core.GmailClient;
+import client.core.common.ReceivedMessage;
+import client.core.interfaces.IReceiver;
 import employee.Employee;
 import exceptionsLogger.ExceptionsLogger;
 import letterLogic.Letter;
@@ -12,59 +12,83 @@ import parser.LetterTypeChecker;
 import parser.Parser;
 import parser.ParserJson;
 
+import javax.mail.MessagingException;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Controller {
-	private Timer updateTimer;
-	private TimerTask getNewLettersTask;
-	private Parser parser = new Parser();
-	private ArrayList<Letter> letters = new ArrayList<>();
-	private ArrayList<Employee> employees;
-	private ExceptionsLogger logger = new ExceptionsLogger("D:/serverExceptions.log");
-	private int timerUpdatePeriod = 10 * 60 * 1000;
-	private LetterTypeChecker letterTypeChecker = new LetterTypeChecker();
- 	
- 	
-	public static void main(String[] args) {
-		
-		
-		Controller controller = new Controller();
-		controller.runServer();
-		
-	}
-	
-	
-	private void runServer() {
-		employees =  initializeEmployeesCollection();
-		
-		updateTimer = new Timer();
-		getNewLettersTask = new TimerTask() {
-			public void run() {
-				// TODO get new letters method()
-				 
-				if(letterTypeChecker.IsRequest(content, isAttachment) == LetterType.ANSWER) {
-					// **** or setAnswer to letter which id that same that in letter
-					
-				}
-				else if(letterTypeChecker.IsRequest(content, isAttachment) == LetterType.REQUEST) {
-					// **** parse xls create Letter objects and add it to letters
-				}
-				else {
-					sentBadLetterTypeError(senderEmail);
-				}
-			}
-		};
-		
-		updateTimer.scheduleAtFixedRate(getNewLettersTask, 0, timerUpdatePeriod);
-	}
-	
-	private ArrayList<Employee> initializeEmployeesCollection() {
-		
-		ArrayList<Employee> employees = null;
-		try {
-		employees = new ParserJson().parseJSON("C:\\Users\\Ігор\\Desktop\\employees.json");
-		}
-		catch(Exception e) {
-			logger.log(e);
-		}
-		return employees;
-	}
+
+    private Parser parser = new Parser();
+    private ArrayList<Letter> letters = new ArrayList<>();
+    private ArrayList<Employee> employees;
+    private ExceptionsLogger logger = new ExceptionsLogger("D:/serverExceptions.log");
+    private LetterTypeChecker letterTypeChecker = new LetterTypeChecker();
+
+
+    public static void main(String[] args) {
+
+
+        Controller controller = new Controller();
+        controller.runServer();
+
+    }
+
+
+    private void runServer() {
+        employees = initializeEmployeesCollection();
+        client().receive(new IReceiver.ReceiveCallback() {
+            @Override
+            public void onReceive(Set<ReceivedMessage> messages) {
+                System.out.println("Received messages: " + messages
+                        .stream()
+                        .map(m ->  " => " + m.getDate())
+                        .collect(Collectors.joining("\n"))
+                );
+            }
+
+            @Override
+            public void onUpdate(ReceivedMessage message) {
+                System.out.println("New message: " + message.getMessage() + " => " + message.getDate());
+                if (letterTypeChecker.IsRequest(message.getMessage(),
+                        message.getAttachment()==null) == LetterType.ANSWER) {
+                    // **** or setAnswer to letter which id that same that in letter
+
+                } else if (letterTypeChecker.IsRequest(message.getMessage(),
+                        message.getAttachment()!=null) == LetterType.REQUEST) {
+                    // **** parse xls create Letter objects and add it to letters
+                } else {
+                    sentBadLetterTypeError(message.getFrom());
+                }
+            }
+
+            @Override
+            public void onError(MessagingException e) {
+                logger.log(e);
+            }
+        });
+
+
+    }
+
+
+    private ArrayList<Employee> initializeEmployeesCollection() {
+
+        ArrayList<Employee> employees = null;
+        try {
+            employees = new ParserJson().parseJSON("C:\\Users\\пїЅпїЅпїЅпїЅ\\Desktop\\employees.json");
+        } catch (Exception e) {
+            logger.log(e);
+        }
+        return employees;
+    }
+
+    public GmailClient client() {
+        GmailClient client = GmailClient.get()
+                .loginWith(Gmail.auth("", ""))
+                .beforeLogin(() -> System.out.println("Login..."))
+                .onLoginError(e -> logger.log(e))
+                .onLoginSuccess(() -> System.out.println("Success login!"));
+        return client.auth();
+    }
 }
